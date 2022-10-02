be6f3b710d19e745737a4c45f333f1de94e538c4
a-rod85 <31aaronanderson@gmail.com>
2022-10-02 09:13:52 +0000

    add to testviews.py

--- todo/tests_views.py
+++ todo/tests_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from .models import Item
 
+
 class TestViews(TestCase):
 
     def test_get_todo_list(self):
@@ -30,10 +31,16 @@ class TestViews(TestCase):
         existing_items = Item.objects.filter(id=item.id)
         self.assertCountEqual(len(exsiting_items), 0)
         
-
     def test_can_toggle_item(self): 
         item = Item.objects.create(name='Test Todo Item', done=True)
         response = self.client.get(f'/delete/{item.id}')
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)    
-        self.assertFalse(updated_item.done)          
+        self.assertFalse(updated_item.done)  
+
+    def test_can_edit_item(self):  
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Update Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertFalse(updated_item.name, 'Updated Name')
